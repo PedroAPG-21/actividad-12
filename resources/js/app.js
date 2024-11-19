db152b955cde9f6d311b5a66de0e3be0c677063c
APGPeter <43098755+APGPeter@users.noreply.github.com>
2024-11-18 23:18:41 -0600

    Primer commit del proyecto ChuckJokesVue

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'; // Vue 3
+
+import App from 'C:\Users\pedro\actividad-12\resources\js\components\ChuckJokes.vue'; 
+
+createApp(App).mount('#app');
+
---
+++ resources/js/components/ChuckJokes.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="chuck-jokes">
+      <h1>Chuck Norris Jokes</h1>
+      <ul>
+        <li v-for="(joke, index) in jokes" :key="index" class="joke-item">
+          {{ joke.value }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        jokes: [
+          { value: "Chuck Norris can skydive into outer space." },
+          { value: "The chief export of Chuck Norris is pain." },
+          { value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
+          { value: "Time waits for no man. Unless that man is Chuck Norris." },
+          { value: "If you spell Chuck Norris in Scrabble, you win. Forever." },
+        ]
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .chuck-jokes {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f9;
+    padding: 20px;
+    text-align: center;
+  }
+  
+  h1 {
+    color: #333;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  .joke-item {
+    background-color: #fff;
+    margin: 10px 0;
+    padding: 15px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    font-size: 16px;
+    color: #555;
+  }
+  
+  .joke-item:hover {
+    background-color: #e7f7ff;
+    cursor: pointer;
+  }
+  </style>
+  
